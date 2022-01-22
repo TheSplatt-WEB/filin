9e5fe94acdda4d8c83f140e5470d569a831de37e
TheSplatt <b-26_sokol@mail.ru>
2022-01-22 22:47:37 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function testWebP(callback) {
   var webP = new Image();
 
@@ -104,4 +110,45 @@ $(function () {
       }
     });
   });
+  $('.to-main__btn').on('click', function () {
+    $('.to-main__wrapper').toggleClass('open');
+    $('body').toggleClass('lock');
+  });
+  $('.to-main__close').on('click', function () {
+    $('.to-main__wrapper').removeClass('open');
+    $('body').removeClass('lock');
+  });
 });
+var questionsLink = document.querySelectorAll('.accordion__btn');
+
+var _iterator = _createForOfIteratorHelper(questionsLink),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var questionsLinkItem = _step.value;
+    questionsLinkItem.addEventListener('click', function (el) {
+      var _this = this;
+
+      this.classList.toggle('open');
+
+      var dropHidden = function dropHidden() {
+        return _this.classList.contains('open');
+      };
+
+      if (dropHidden()) {
+        this.nextElementSibling.querySelector('.accordion__descr').style.height = this.nextElementSibling.querySelector('.accordion__descr').scrollHeight + 15 + 'px';
+        this.nextElementSibling.querySelector('.accordion__descr').classList.add('open');
+      } else {
+        this.nextElementSibling.querySelector('.accordion__descr').style.height = 0 + 'px';
+        this.nextElementSibling.querySelector('.accordion__descr').classList.remove('open');
+      }
+    });
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
+
+;
